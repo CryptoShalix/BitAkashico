8c906c29090a3074c49ae1baf6c6306bf77a01c1
Adlrg <adlrg.developer@gmail.com>
2022-09-19 20:06:09 +0200

    Updated game links

--- src/app/shared/models/core.ts
+++ src/app/shared/models/core.ts
@@ -30,14 +30,14 @@ export enum URLS {
   REF_GAME_AND_TURBO84 = 'https://turbo84.sng.link/Dbgmr/9tud9/9wff',
   REF_GAME_AND_SNAKE = 'https://bitcoin-snake.sng.link/Diskp/geum/3abn',
   REF_GAME_AND_BAY = 'https://thndr-bay.sng.link/Dk8fg/s8iy/vbo5',
-  REF_GAME_AND_SOLITAIRE = 'https://solitaire.thndr.games/r/KPoa',
+  REF_GAME_AND_SOLITAIRE = 'https://bitcoin-solitaire.sng.link/Dx606/hp39/kvzf',
   REF_GAME_AND_MINER = 'https://play.google.com/store/apps/details?id=com.fumbgames.bitcoinminor',
 
   REF_GAME_IOS_BOUNCE = 'https://bitcoin-bounce.sng.link/Ddomi/01rz/u50x',
   REF_GAME_IOS_TURBO84 = 'https://turbo84.sng.link/Dbgmr/b7kl/8vys',
   REF_GAME_IOS_SNAKE = 'https://bitcoin-snake.sng.link/Diskp/q2fr/u2ex',
   REF_GAME_IOS_BAY = 'https://thndr-bay.sng.link/Dk8fg/5lrf/d0c9',
-  REF_GAME_IOS_SOLITAIRE = 'https://solitaire.thndr.games/r/KPoa',
+  REF_GAME_IOS_SOLITAIRE = 'https://bitcoin-solitaire.sng.link/Dx606/kbea/04tp',
   REF_GAME_IOS_MINER = 'https://apps.apple.com/us/app/bitcoin-miner-idle-tycoon/id1413770650',
 
   // REFERRALS: ANALYSIS MARKET
